ff24ef7179e597322f2ed7dbbdb40c75be01afb8
AdrianRaducanu <48106661+AdrianRaducanu@users.noreply.github.com>
2024-01-04 20:41:37 +0200

    starting page layout + style

--- main.js
+++ main.js
@@ -5,11 +5,17 @@ import ClientDataGetter from "./src/ClientDataGetter.js";
 import Engine from "./src/Engine.js";
 
 
-const engineCanvas = document.getElementById('canvas');
-Engine.instance.initialize(canvas, () => {ClientDataGetter.setWeatherAndTime();});
 
-DomManagement.instance.initialize();
-SoundManagement.instance.initialize();
+
+const startGame = () => {
+    const engineCanvas = document.getElementById('canvas');
+    Engine.instance.initialize(engineCanvas, () => {ClientDataGetter.setWeatherAndTime();});
+
+    DomManagement.instance.initialize();
+    SoundManagement.instance.initialize();
+}
+
+// startGame()
 
 window.Manager = Manager;
 
